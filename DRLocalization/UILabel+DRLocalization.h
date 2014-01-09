5dd885b0d290f9f05e3dbc04bf8f05096e51108c
Darrarski <darrarski@gmail.com>
2014-01-09 01:53:58 +0100

    UILabel category for setting up localization using interface builder without need to set object’s localization keyPath

---
+++ DRLocalization/UILabel+DRLocalization.h
@@ -0,0 +1,13 @@
+//
+//  UILabel+DRLocalization.h
+//  DRLocalizationDemo
+//
+//  Created by Dariusz Rybicki on 09.01.2014.
+//  Copyright (c) 2014 Darrarski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (DRLocalization)
+
+@end
---
+++ DRLocalization/UILabel+DRLocalization.m
@@ -0,0 +1,27 @@
+//
+//  UILabel+DRLocalization.m
+//  DRLocalizationDemo
+//
+//  Created by Dariusz Rybicki on 09.01.2014.
+//  Copyright (c) 2014 Darrarski. All rights reserved.
+//
+
+#import "UILabel+DRLocalization.h"
+#import "DRLocalization.h"
+
+@implementation UILabel (DRLocalization)
+
+- (void)DRLocalize
+{
+	NSString *key = self.DRLocalizationKey;
+	NSString *keyPath = self.DRLocalizationKeyPath;
+	
+	if (key && !keyPath) {
+		self.text = [[DRLocalization sharedInstance] stringForKey:key];
+	}
+	else {
+		[super DRLocalize];
+	}
+}
+
+@end
