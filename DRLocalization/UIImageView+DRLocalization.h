6ca52695216b16c3bfdc4ddbb40b8ed671a743a2
Darrarski <darrarski@gmail.com>
2014-01-16 00:33:42 +0100

    Added UIImageView localization category

---
+++ DRLocalization/UIImageView+DRLocalization.h
@@ -0,0 +1,13 @@
+//
+//  UIImageView+DRLocalization.h
+//  DRLocalizationDemo
+//
+//  Created by Dariusz Rybicki on 16.01.2014.
+//  Copyright (c) 2014 Darrarski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (DRLocalization)
+
+@end
---
+++ DRLocalization/UIImageView+DRLocalization.m
@@ -0,0 +1,29 @@
+//
+//  UIImageView+DRLocalization.m
+//  DRLocalizationDemo
+//
+//  Created by Dariusz Rybicki on 16.01.2014.
+//  Copyright (c) 2014 Darrarski. All rights reserved.
+//
+
+#import "UIImageView+DRLocalization.h"
+#import "DRLocalization.h"
+
+@implementation UIImageView (DRLocalization)
+
+- (void)DRLocalize
+{
+	NSString *key = self.DRLocalizationKey;
+	NSString *keyPath = self.DRLocalizationKeyPath;
+	
+	if (key && !keyPath) {
+		NSString *imageName = [[DRLocalization sharedInstance] stringForKey:key];
+		UIImage *image = [UIImage imageNamed:imageName];
+		[self setImage:image];
+	}
+	else {
+		[super DRLocalize];
+	}
+}
+
+@end
